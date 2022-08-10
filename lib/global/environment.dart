3a6c8636b597d306467ed85fe0394359f68221c2
Alejandro Ardila Ardila <alejandroardilaardila@MacBook-Air-de-Alejandro.local>
2022-08-10 07:13:06 -0500

    login

--- lib/global/environment.dart
+++ lib/global/environment.dart
@@ -1,5 +1,5 @@
 class Environment {
   //static String apiUrl = 'http://alejandro-erp.waplicaciones.co/api/index.php/';
-  static String apiUrl = 'https://erp.importsoluciones.com/api/index.php/';
+  static String apiUrl = 'http://dev.asocoldro.com/user/login';
   static String frontUrl = 'https://erp.importsoluciones.com/';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:maps_app/screen/screens.dart';
 import 'package:maps_app/blocs/blocs.dart';
+import 'package:maps_app/routes/routes.dart';
+import 'package:maps_app/services/auth_services.dart';
+import 'package:maps_app/services/solicitudes_services.dart';
 
 void main() {
   //runApp(const MapsApp());
@@ -21,10 +25,17 @@ class MapsApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      home: LoadingScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthService()),
+        ChangeNotifierProvider(create: (_) => SolicitudesServices())
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        initialRoute: 'loading',
+        routes: appRoutes,
+      ),
     );
   }
 }
